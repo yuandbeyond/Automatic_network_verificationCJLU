2fa733d7b7e8150068c334e89bc41985e2fb22e5
cassiel0z <cassielz0607@gmail.com>
2023-03-15 15:24:27 +0800

    适配新的网络验证接口

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
+import configparser
 import os
 
 from net_authenticate import net_authenticate
 from vpn_authenticate import vpn_authenticate
 
+
+def check():
+    cfr = configparser.ConfigParser()
+    cfr.read('conf.ini')
+    for section in cfr.sections():
+        for option in cfr.options(section):
+            if cfr.get(section, option) == '':
+                return False
+    return True
+
+
 if __name__ == '__main__':
-    if net_authenticate():
-        vpn_authenticate()
+    if not os.path.isfile("conf.ini"):
+        print('请在同一目录下的conf.ini文件中完成登录信息后再次运行')
+        with open('conf.ini', 'w') as f:
+            cfr = configparser.ConfigParser()
+            cfr.read('conf.ini')
+            cfr.add_section('user')
+            cfr.add_section('vpn')
+            cfr.set('user', 'account', '')
+            cfr.set('user', 'password', '')
+            cfr.set('vpn', 'name', '')
+            cfr.set('vpn', 'username', '')
+            cfr.set('vpn', 'password', '')
+            cfr.write(f)
     else:
-        os.system('pause')
+        if check():
+            if net_authenticate():
+                vpn_authenticate()
+            else:
+                os.system('pause')
+
+        else:
+            print('请在同一目录下的conf.ini文件中完成登录信息后再次运行')
--- net_authenticate.py
+++ net_authenticate.py
@@ -5,63 +5,47 @@ import requests
 
 from get_ip import get_host_ip
 
+import configparser
+
+
 
 def net_authenticate():
-    file_path = 'login_info.txt'
-    flag = True
-    if os.path.exists(file_path):
-        sz = os.path.getsize(file_path)
-        if not sz:
-            flag = False
-    else:
-        flag = False
+        cfr = configparser.ConfigParser()
+        cfr.read('conf.ini')
+        ip = get_host_ip()
+        account = cfr.get('user', 'account')
+        password = cfr.get('user', 'password')
 
-    if flag:
-        with open("login_info.txt", 'r') as file:
-            account = file.readline()
-            account = account.replace("\n", "")
-            password = file.readline()
-            password = password.replace("\n", "")
-    else:
-        with open("login_info.txt", 'w') as file:
-            print('请输入学号:')
-            account = '__' + input()
-            file.write(account + '\n')
-            password = getpass.getpass("请输入密码：\n")
-            file.write(password + '\n')
 
-    ip = get_host_ip()
-    account = account.replace("\n", "")
-    password = password.replace("\n", "")
-    print(account)
-    print(password)
-    header = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 '
-                      'Safari/537.36',
-    }
-    data = {
-        'DDDDD': account,
-        'upass': password,
-        'R1': '0',
-        'R2': '',
-        'R6': '0',
-        'para': '00',
-        '0MKKey': '123456',
-    }
+        header = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 '
+                          'Safari/537.36',
+        }
+        params = {
+            'callback': 'dr1003',
+            'user_account': ',0,__' + account,
+            'user_password': password,
+            'wlan_user_ip': ip,
+            'wlan_user_ipv6': '',
+            'wlan_user_mac': '000000000000',
+            'wlan_ac_ip': '',
+            'wlan_ac_name': '',
+            'jsVersion': '4.2.1',
+            'terminal_type': '1',
+            'v': '7420',
+            'lang': 'en'
+        }
 
-    try:
-        print('正在验证...')
-        requests.post(
-            url='https://portal2.cjlu.edu.cn:801/eportal/?c=ACSetting&a=Login&wlanuserip={}&wlanacip=null&wlanacname=&port=&iTermType=1&mac=000000000000&ip={}&redirect=null=attention&jsonp=jsonp&callback=__jp5'.format(
-                ip, ip), headers=header, data=data, verify=False)
-        print('登陆成功')
+        try:
+            print('正在验证...')
+            res = requests.post(
+                url='https://portal2.cjlu.edu.cn:802/eportal/portal/login', headers=header, params=params)
+            print('登陆成功')
+            return True
 
-        return True
-
-    except requests.exceptions.ProxyError as err:
-        print('未连接校园网或已登录')
-        print('错误信息', err)
-        return False
+        except requests.exceptions.ConnectionError as err:
+            print('未连接校园网或已登录')
+            return False
 
 
 if __name__ == '__main__':
